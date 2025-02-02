b02bf0e47369c8ebb31086b4933774ac4357cc22
victorpoliushko <victor.poliushko@gmail.com>
2025-02-02 19:09:03 +0200

    create apikey table, hash apikey

--- invapp_base/apps/api/src/services/api-key/ApiKeyService.ts
+++ invapp_base/apps/api/src/services/api-key/ApiKeyService.ts
@@ -1,11 +1,40 @@
-import { getRandomString } from "../../helpers/generateString";
+import { pbkdf2Sync } from 'crypto';
+import { getRandomString } from '../../helpers/generateString';
+import { PrismaClient } from '@prisma/client';
 
-
+export interface ApiKeyResponse {
+  apiKey: string;
+}
 
 export class ApiKeyService {
+  private prisma: PrismaClient;
 
-  async create({ userId }: { userId: string }): Promise<ApiKeyResponce> {
+  public constructor(private readonly prismaClient: PrismaClient) {
+    this.prisma = prismaClient;
+  }
 
+  async create({ userId }: { userId: string }): Promise<ApiKeyResponse> {
+    const apiKey = this.generateApiKey();
+    const hashedKey = this.hashApiKey(apiKey);
+    await this.prisma.apiKey.deleteMany({ where: { userId } });
+
+    await this.prisma.apiKey.create({
+      data: {
+        hashedKey,
+        userId
+      }
+    });
+
+    return { apiKey };
+  }
+
+  private hashApiKey(key: string): string {
+    const algorithm = 'sha256';
+    const iterations = 100000;
+    const keyLength = 64;
+    return pbkdf2Sync(key, '', iterations, keyLength, algorithm).toString(
+      'hex',
+    );
   }
 
   private generateApiKey() {
@@ -15,7 +44,6 @@ export class ApiKeyService {
     //   acc[chunkIndex] = (acc[chunkIndex] || '') + char;
     //   return acc;
     // }, []).join('-');
-
 
     // option 2
     const randomStr = getRandomString(32);
