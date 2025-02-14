4582c31a8682450ae05ec6647f9e5d2272496487
victorpoliushko <victor.poliushko@gmail.com>
2025-02-14 14:48:09 +0200

    protfolio and Symbol WIP

--- invapp_base/apps/api/src/investStrategies/investStrategies.service.ts
+++ invapp_base/apps/api/src/investStrategies/investStrategies.service.ts
@@ -1,29 +1,29 @@
-// import { HttpException, Injectable } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { Model } from 'mongoose';
-// import { InvestStrategy } from 'src/schemas/investSrategy.schema';
-// import { CreateInvestStrategyDto } from './dto/CreateInvestStrategyDto';
-// import { User } from 'src/schemas/user.schema';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { InvestStrategy } from 'src/schemas/investSrategy.schema';
+import { CreateInvestStrategyDto } from './dto/CreateInvestStrategyDto';
+import { User } from 'src/schemas/user.schema';
 
-// @Injectable()
-// export class InvestStrategiesService {
-//   constructor(
-//     @InjectModel(InvestStrategy.name)
-//     private investStrategyModel: Model<InvestStrategy>,
-//     @InjectModel(User.name)
-//     private userModel: Model<User>,
-//   ) {}
-//   async create(createInvestStrategyDto: CreateInvestStrategyDto) {
-//     const { userId } = createInvestStrategyDto;
-//     const user = await this.userModel.findById(userId);
-//     if (!user) throw new HttpException('user not found', 404);
-//     const strat = new this.investStrategyModel(createInvestStrategyDto);
-//     const savedStrat = await strat.save();
-//     await user.updateOne({
-//       $push: {
-//         investStrategies: savedStrat._id
-//       }
-//     })
-//     return savedStrat;
-//   }
-// }
+@Injectable()
+export class InvestStrategiesService {
+  constructor(
+    @InjectModel(InvestStrategy.name)
+    private investStrategyModel: Model<InvestStrategy>,
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+  async create(createInvestStrategyDto: CreateInvestStrategyDto) {
+    const { userId } = createInvestStrategyDto;
+    const user = await this.userModel.findById(userId);
+    if (!user) throw new HttpException('user not found', 404);
+    const strat = new this.investStrategyModel(createInvestStrategyDto);
+    const savedStrat = await strat.save();
+    await user.updateOne({
+      $push: {
+        investStrategies: savedStrat._id
+      }
+    })
+    return savedStrat;
+  }
+}
