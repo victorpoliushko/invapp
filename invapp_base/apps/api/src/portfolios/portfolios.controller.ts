b931ceb14b8970f97f63f4f49642754b303e5388
victorpoliushko <victor.poliushko@gmail.com>
2025-02-20 19:22:11 +0200

    fixing portfolio creation for another user

--- invapp_base/apps/api/src/portfolios/portfolios.controller.ts
+++ invapp_base/apps/api/src/portfolios/portfolios.controller.ts
@@ -1,17 +1,37 @@
-import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
-import { PortfoliosService } from "./portfolios.service";
-import { AuthGuard } from "@nestjs/passport";
-import { CreatePortfolioDto } from "./dto/CreatePortfolio.dto";
-
+import {
+  Body,
+  Controller,
+  HttpException,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { PortfoliosService } from './portfolios.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreatePortfolioDto } from './dto/CreatePortfolio.dto';
+import { GetUser } from '../auth/decorators/GetUser.decorator';
+import { User } from '@prisma/client';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 
 @Controller('portfolios')
 export class PortfoliosController {
   constructor(private portfoliosService: PortfoliosService) {}
-  
+
   @Post()
   @UseGuards(AuthGuard('jwt'))
   @UsePipes(new ValidationPipe())
-  createPortfolio(@Body() createPortfolioDto: CreatePortfolioDto) {
+  createPortfolio(
+    @Body() createPortfolioDto: CreatePortfolioDto,
+    @GetUser() user: User,
+  ) {
+    check userid, forbiden all the time
+    if (createPortfolioDto.userId !== user.id) {
+      throw new HttpException(
+        getReasonPhrase(StatusCodes.FORBIDDEN),
+        StatusCodes.FORBIDDEN,
+      );
+    }
     return this.portfoliosService.create(createPortfolioDto);
   }
 }
