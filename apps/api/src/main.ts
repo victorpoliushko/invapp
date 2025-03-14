b67267e7fd8e5d0996e4e463da6bbb5ef0a08255
victorpoliushko <victor.poliushko@gmail.com>
2025-03-14 14:30:52 +0200

    configure api proxy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
+  app.setGlobalPrefix('api');
   app.enableCors(); 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
