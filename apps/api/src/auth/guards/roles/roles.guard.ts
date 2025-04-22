8d311ebf1abdfc0f51d559bbd3ceb945b775bc15
victorpoliushko <victor.poliushko@gmail.com>
2025-04-22 18:48:40 +0300

    WIP roles guard

---
+++ apps/api/src/auth/guards/roles/roles.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Role } from "@prisma/client";
+import { Observable } from "rxjs";
+import { ROLES_KEY } from "src/auth/decorators/roles.decorator";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ])
+    return true;
+  }
+}
