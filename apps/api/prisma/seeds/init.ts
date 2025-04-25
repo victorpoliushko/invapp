56acdc783f5e893b98a7e5184a65e96f21efafe3
victorpoliushko <victor.poliushko@gmail.com>
2025-04-25 10:23:44 +0300

    update seed

--- apps/api/prisma/seeds/init.ts
+++ apps/api/prisma/seeds/init.ts
@@ -1,20 +1,44 @@
 import { PrismaClient, Role } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  // very secure way to create an admin
+  const adminHashedPassword = await bcrypt.hash("admin", 10);
+  const editorHashedPassword = await bcrypt.hash("editor", 10);
+  const userHashedPassword = await bcrypt.hash("user", 10);
+
   console.log("Seeding DB...");
 
-  await prisma.user.create({
-    data: {
-      username: "admin",
-      password: "admin",
-      email: "admin@email.com",
-      role: Role.ADMIN
-    }
-  });
+  await Promise.all([
+    await prisma.user.create({
+      data: {
+        username: "admin",
+        password: adminHashedPassword,
+        email: "admin@email.com",
+        role: Role.ADMIN
+      }
+    }),
+    await prisma.user.create({
+      data: {
+        username: "edtior",
+        password: editorHashedPassword,
+        email: "editor@email.com",
+        role: Role.EDITOR
+      }
+    }),
+    await prisma.user.create({
+      data: {
+        username: "user",
+        password: userHashedPassword,
+        email: "user@email.com",
+        role: Role.USER
+      }
+    })
+  ]);
 
-   console.log("Seeding finished")
+  console.log("Seeding finished")
 }
 
 main().then(async () => {
--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -17,6 +17,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return this.authService.validateJwtuser(payload.userId);
+    return this.authService.validateJwtUser(payload.userId);
   }
 }
