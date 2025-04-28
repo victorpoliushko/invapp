ae6a8d903c641798dbd10eeaf3211586fdb1c0a6
victorpoliushko <victor.poliushko@gmail.com>
2025-04-28 12:02:57 +0300

    fix roles guard, prepare for google oauth

--- apps/api/src/auth/guards/roles/roles.guard.ts
+++ apps/api/src/auth/guards/roles/roles.guard.ts
@@ -13,7 +13,8 @@ export class RolesGuard implements CanActivate {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass()
-    ])
+    ]);
+    if (!requiredRoles) return true;
     const user = context.switchToHttp().getRequest().user;
     const hasRequiredRole = requiredRoles.some(role => role === user.role);
     return hasRequiredRole;
