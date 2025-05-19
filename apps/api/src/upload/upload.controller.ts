0348af03e4188cf64a96370f79b7ff6d59e5b35c
victorpoliushko <victor.poliushko@gmail.com>
2025-05-19 19:47:58 +0300

    tmp file storage using multer

--- apps/api/src/upload/upload.controller.ts
+++ apps/api/src/upload/upload.controller.ts
@@ -1,11 +1,24 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('upload')
 export class UploadController {
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: "./tmpStorage",
+      filename: ((req, file, cb) => {
+        const uniqueSufix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, `${file.fieldname}-${uniqueSufix}${extname(file.originalname)}`);
+      })
+    })
+  }))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file)
+    return {
+      originalname: file.originalname,
+      filename: file.filename
+    }
   }
 }
