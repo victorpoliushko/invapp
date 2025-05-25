6c43a2c397214a5dd421ea77754c2fe3e9e90bec
victorpoliushko <victor.poliushko@gmail.com>
2025-05-25 11:12:14 +0300

    i want to die

--- apps/client/src/components/realEstate/RealEstate.tsx
+++ apps/client/src/components/realEstate/RealEstate.tsx
@@ -1,3 +1,5 @@
+import "./RealEstate.css";
+
 export default function RealEstate(props: {
   title: string;
   imageSrc: string;
@@ -7,39 +9,11 @@ export default function RealEstate(props: {
   return (
     <div className="real-estate">
       <article className="real-estate-card">
-        <img src="{props.imageSrc}" alt="house one" />
+        <img src={props.imageSrc} alt="house one" />
         <h3>{props.title}</h3>
         <div className="info-group">
           <p>{props.info}</p>
           <p>{props.price}</p>
-        </div>
-      </article>
-      <article className="real-estate-card">
-        <img src="../../assets/house2.jpeg" alt="house one" />
-        <h3>Two bedroom flat, Kyiv, Bobik str.</h3>
-        <div className="info-group">
-          <p>Used house with some stuff</p>
-          <p>$95000</p>
-        </div>
-        <div className="info-group">
-          <p>Used house with some stuff</p>
-          <p>$95000</p>
-        </div>
-      </article>
-      <article className="real-estate-card">
-        <img src="../../assets/house3.jpeg" alt="house one" />
-        <h3>Three bedroom flat, Rivne, Boobik str.</h3>
-        <div className="info-group">
-          <p>New house with some stuff</p>
-          <p>$235000</p>
-        </div>
-      </article>
-      <article className="real-estate-card">
-        <img src="../../assets/house3.jpeg" alt="house one" />
-        <h3>No bedroom house, Odesa, Valik str.</h3>
-        <div className="info-group">
-          <p>No stuff, just walls</p>
-          <p>$15000</p>
         </div>
       </article>
     </div>
