c9a2873df4b3d128588b7af94a17677ef87d31b4
victorpoliushko <victor.poliushko@gmail.com>
2025-05-26 19:29:12 +0300

    same

--- apps/client/src/components/realEstate/RealEstate.tsx
+++ apps/client/src/components/realEstate/RealEstate.tsx
@@ -1,21 +1,19 @@
 import "./RealEstate.css";
 
-export default function RealEstate(props: {
+export default function RealEstate({ title, imageSrc, info, price }: {
   title: string;
   imageSrc: string;
   info: string;
   price: string;
 }) {
   return (
-    <div className="real-estate">
-      <article className="real-estate-card">
-        <img src={props.imageSrc} alt="house one" />
-        <h3>{props.title}</h3>
-        <div className="info-group">
-          <p>{props.info}</p>
-          <p>{props.price}</p>
-        </div>
-      </article>
-    </div>
+    <article className="real-estate-card">
+      <img src={imageSrc} alt="house one" />
+      <h3>{title}</h3>
+      <div className="info-group">
+        <p>{info}</p>
+        <p>{price}</p>
+      </div>
+    </article>
   );
 }
