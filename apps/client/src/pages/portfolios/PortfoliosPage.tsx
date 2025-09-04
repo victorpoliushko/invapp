508805c8f9ed7f8f9e841fea07df1e1ded05e3f3
victorpoliushko <victor.poliushko@gmail.com>
2025-09-04 18:48:53 +0300

    make gap smaller

--- apps/client/src/pages/portfolios/PortfoliosPage.tsx
+++ apps/client/src/pages/portfolios/PortfoliosPage.tsx
@@ -59,6 +59,54 @@ export default function PortfoliosPage() {
           </div>
         </div>
       </div>
+
+       <div className="portfolio-min">
+        <h2>Gain</h2>
+        <div className="portfolio-min-cols">
+          <div className="portfolio-min-col">
+            <h4>Returns</h4>
+            <p>Goal: 6%</p>
+            <p>Actual: 11.2%</p>
+            <p>$: 10000</p>
+          </div>
+          <div className="portfolio-min-col">
+            <h4>Top performers</h4>
+            <p>VOO 13% +$7605</p>
+            <p>NEAR 10% +$800</p>
+            <p>GOOG 8% +$600</p>
+          </div>
+          <div className="portfolio-min-col">
+            <h4>Losers</h4>
+            <p>ADA -2% +$50</p>
+            <p>PLTR 4% +$50</p>
+            <p>SCHD 1$ +15%</p>
+          </div>
+        </div>
+      </div>
+
+       <div className="portfolio-min">
+        <h2>Gain</h2>
+        <div className="portfolio-min-cols">
+          <div className="portfolio-min-col">
+            <h4>Returns</h4>
+            <p>Goal: 6%</p>
+            <p>Actual: 11.2%</p>
+            <p>$: 10000</p>
+          </div>
+          <div className="portfolio-min-col">
+            <h4>Top performers</h4>
+            <p>VOO 13% +$7605</p>
+            <p>NEAR 10% +$800</p>
+            <p>GOOG 8% +$600</p>
+          </div>
+          <div className="portfolio-min-col">
+            <h4>Losers</h4>
+            <p>ADA -2% +$50</p>
+            <p>PLTR 4% +$50</p>
+            <p>SCHD 1$ +15%</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
