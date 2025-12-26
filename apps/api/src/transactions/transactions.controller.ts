bc17ec63d7a4f7aa4339f4f2521a56546d5a46c3
victorpoliushko <victor.poliushko@gmail.com>
2025-12-26 14:38:31 +0200

    WIP create transaction

--- apps/api/src/transactions/transactions.controller.ts
+++ apps/api/src/transactions/transactions.controller.ts
@@ -26,5 +26,6 @@ export class TransactionsController {
   addTransation(
     @Body() createTransactionDTO: CreateTransactionDto,
     @GetUser() user: User,
-  ) {}
+  ) {
+  }
 }
--- apps/api/src/transactions/transactions.service.ts
+++ apps/api/src/transactions/transactions.service.ts
@@ -1,1 +1,18 @@
-export class TransactionsService {}
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateTransactionDto } from "./dto/CreateTransaction.dto";
+import { TransactionsDto } from "./dto/Transations.dto";
+import { plainToInstance } from "class-transformer";
+
+export class TransactionsService {
+  constructor(
+    private prismaService: PrismaService
+  ) {}
+
+  async create(input: CreateTransactionDto): Promise<TransactionsDto> {
+    const createdTransaction = await this.prismaService.transaction.create({
+      data: input
+    });
+
+    return plainToInstance(TransactionsDto, createdTransaction);
+  }
+}
