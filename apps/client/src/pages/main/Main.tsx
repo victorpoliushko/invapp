708177ef85dc0a01a9026703bc1f79b07bf40ee0
victorpoliushko <victor.poliushko@gmail.com>
2026-04-06 11:42:31 +0300

    change css

--- apps/client/src/pages/main/Main.tsx
+++ apps/client/src/pages/main/Main.tsx
@@ -10,33 +10,8 @@ export const MainPage = () => {
         {/* <MarketHeatmap /> */}
         
       </div>
-      <section className="news-section section-container section-flex-container">
-        {/* <h1>News</h1> */}
-        <MarketNews />
-        {/* <div className="news-div">
-          <div className="news-item">
-            <h3>
-              VOO Continues Strong Performance Amid S&P 500 Records The Vanguard
-              S&P 500 ETF (VOO) continues to show robust performance, reaching
-              new highs in tandem with the broader S&P 500 Index. The fund's
-              year-to-date returns have been strong, mirroring the impressive
-              run in the U.S. stock market.
-            </h3>
-          </div>
-          <div className="news-item">
-            <h3>
-              Ethereum Continues to Gain Institutional Traction The Ethereum
-              network and its native cryptocurrency, Ether (ETH), have been at
-              the center of growing institutional interest, particularly with
-              the recent launch of spot Ethereum ETFs in the U.S. These new
-              investment products have attracted billions in inflows, signaling
-              Wall Street's increasing appetite for the asset.
-            </h3>
-          </div>
-        </div> */}
-      </section>
 
-      <section className="stocks-section section-container">
+            <section className="stocks-section section-container">
         <h1>Market data</h1>
         <MarketTicker />
         {/* <h1>Stocks</h1>
@@ -96,6 +71,33 @@ export const MainPage = () => {
           </div>
         </div> */}
       </section>
+      <section className="news-section section-container section-flex-container">
+        {/* <h1>News</h1> */}
+        <MarketNews />
+        {/* <div className="news-div">
+          <div className="news-item">
+            <h3>
+              VOO Continues Strong Performance Amid S&P 500 Records The Vanguard
+              S&P 500 ETF (VOO) continues to show robust performance, reaching
+              new highs in tandem with the broader S&P 500 Index. The fund's
+              year-to-date returns have been strong, mirroring the impressive
+              run in the U.S. stock market.
+            </h3>
+          </div>
+          <div className="news-item">
+            <h3>
+              Ethereum Continues to Gain Institutional Traction The Ethereum
+              network and its native cryptocurrency, Ether (ETH), have been at
+              the center of growing institutional interest, particularly with
+              the recent launch of spot Ethereum ETFs in the U.S. These new
+              investment products have attracted billions in inflows, signaling
+              Wall Street's increasing appetite for the asset.
+            </h3>
+          </div>
+        </div> */}
+      </section>
+
+
 
       <section className="realestate-section section-container section-flex-container">
         <h1>Real estate</h1>
