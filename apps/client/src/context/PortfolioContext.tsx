e34ecbd525256b35ca4d3830fd37599c7db693d8
victorpoliushko <victor.poliushko@gmail.com>
2026-04-14 09:54:16 +0300

    provider

--- apps/client/src/context/PortfolioContext.tsx
+++ apps/client/src/context/PortfolioContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useFetchWithRedirect } from "../hooks/useApiWithRedirect";
 
@@ -11,9 +11,15 @@ interface PortfolioContextType {
   refreshData: () => Promise<void>;
 }
 
-const PortfolioContext = createContext<PortfolioContextType | undefined>(undefined);
+const PortfolioContext = createContext<PortfolioContextType | undefined>(
+  undefined,
+);
 
-export const PortfolioProvider = ({ children }: { children: React.ReactNode }) => {
+export const PortfolioProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const { id } = useParams<{ id: string }>();
   const [portfolio, setPortfolio] = useState<any>();
   const [loadingPrices, setLoadingPrices] = useState({});
@@ -22,15 +28,54 @@ export const PortfolioProvider = ({ children }: { children: React.ReactNode }) =
 
   const refreshData = async () => {
     if (!id) return;
-    const res = await fetchWithRedirect(`http://localhost:5173/api/portfolios/${id}`, {
-      headers: { 
-        Authorization: `Bearer ${token}`
-      }
-    });
+    const res = await fetchWithRedirect(
+      `http://localhost:5173/api/portfolios/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
 
     const data = await res.json();
     setPortfolio(data);
-  }
+  };
 
-  
+  const deleteAsset = async (assetId: string) => {
+    await fetch(`api/portfolios/${id}/assets`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        body: JSON.stringify({ assetId }),
+      },
+    });
+
+    await refreshData();
+  };
+
+  useEffect(() => {
+    refreshData();
+  }, [id]);
+
+  return (
+    <PortfolioContext.Provider
+      value={{
+        portfolio,
+        loadingPrices,
+        deleteAsset,
+        refreshData,
+        addTransaction: async () => {},
+        updatePortfolioName: async () => {},
+      }}
+    >
+      {children}
+    </PortfolioContext.Provider>
+  );
 };
+
+export const usePortfolio = () => {
+  const context = useContext(PortfolioContext);
+  if (!context) throw new Error("usePortfolio must be used within PortfolioProvider");
+  return context;
+}
