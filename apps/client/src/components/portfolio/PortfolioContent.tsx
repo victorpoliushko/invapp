39883b7bf3459ee2f1d8e53d66368dc87b2a1d7a
victorpoliushko <victor.poliushko@gmail.com>
2026-04-21 13:24:24 +0300

    moved tabs

--- apps/client/src/components/portfolio/PortfolioContent.tsx
+++ apps/client/src/components/portfolio/PortfolioContent.tsx
@@ -2,18 +2,20 @@ import { usePortfolio } from "../../context/PortfolioContext";
 import { AssetTable } from "./AssetTable";
 import "../../pages/portfolio/PortfolioPage.css";
 import { useEffect, useState } from "react";
-import editIcon from "../../assets/pencil-svgrepo-com.svg"
+import editIcon from "../../assets/pencil-svgrepo-com.svg";
 
 export function PortfolioContent() {
   const { portfolio, updatePortfolioName } = usePortfolio();
 
   const [isEditing, setIsEditing] = useState(false);
   const [newName, setNewName] = useState("");
-  if (!portfolio) return <div>Loading...</div>;
+  const [selectedTab, setSelectedTab] = useState("tab-stocks");
 
   useEffect(() => {
     if (portfolio) setNewName(portfolio.name);
   }, [portfolio]);
+
+  if (!portfolio) return <div>Loading...</div>;
 
   const handleSave = async () => {
     await updatePortfolioName(newName);
@@ -33,7 +35,12 @@ export function PortfolioContent() {
             autoFocus
           />
           <button onClick={handleSave}>Save</button>
-          <button onClick={() => { setIsEditing(false); setNewName(portfolio.name); }}>
+          <button
+            onClick={() => {
+              setIsEditing(false);
+              setNewName(portfolio.name);
+            }}
+          >
             Cancel
           </button>
         </>
@@ -44,14 +51,334 @@ export function PortfolioContent() {
             <button onClick={() => setIsEditing(true)}>
               <img src={editIcon} alt="Edit" height={30} width={30} />
             </button>
-            {/* <button onClick={() => deletePortfolio(portfolio.id)}>
+            {/* `<button onClick={() => deletePortfolio(portfolio.id)}>
               <img src={deleteIcon} alt="Delete" height={30} width={30} />
-            </button> */}
+            </button>` */}
           </div>
         </div>
       )}
-      <div className="tab">
-        <AssetTable assets={portfolio.portfolioAssets} />
+      <div className="assets">
+        <input
+          type="radio"
+          name="tabs"
+          id="tab-stocks"
+          checked={selectedTab === "tab-stocks"}
+          onChange={() => setSelectedTab("tab-stocks")}
+        />
+        <label htmlFor="tab-stocks">Stocks</label>
+
+        <div className="tab">
+          <AssetTable assets={portfolio.portfolioAssets} />
+        </div>
+
+        <input
+          type="radio"
+          name="tabs"
+          id="tab-bonds"
+          onChange={() => setSelectedTab("tab-bonds")}
+          checked={selectedTab === "tab-bonds"}
+        />
+        <label htmlFor="tab-bonds">Bonds</label>
+        <div className="tab">
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Account</th>
+                <th scope="col">Due Date</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Period</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Account">Bond - 3412</td>
+                <td data-label="Due Date">04/01/2016</td>
+                <td data-label="Quantity">$1,190</td>
+                <td data-label="Period">03/01/2016 - 03/31/2016</td>
+              </tr>
+              <tr>
+                <td scope="row" data-label="Account">
+                  Bond - 6076
+                </td>
+                <td data-label="Due Date">03/01/2016</td>
+                <td data-label="Quantity">$2,443</td>
+                <td data-label="Period">02/01/2016 - 02/29/2016</td>
+              </tr>
+              <tr>
+                <td>
+                  {/* <div className="asset-autocomplete">
+                    <input
+                      type="text"
+                      name="asset"
+                      value={searchTerm}
+                      onChange={(e) => {
+                        setAutocompleteEnabled(true);
+                        setSearchTerm(e.target.value);
+                      }}
+                      onKeyDown={(e) => {
+                        // if (e.key === "Enter") handleAddAsset();
+                      }}
+                      required
+                      placeholder="Asset"
+                      autoComplete="off"
+                    />
+                    {suggestions.length > 0 && (
+                      <ul className="suggestions-list">
+                        {suggestions.slice(0, 5).map((s) => (
+                          <li
+                            key={s.assetSymbol}
+                            title={s.name}
+                            onClick={() => {
+                              setSearchTerm(s.assetSymbol);
+                              setNewAsset({
+                                ...newAsset,
+                                assetName: s.assetSymbol,
+                              });
+                              setSuggestions([]);
+                              setAutocompleteEnabled(false);
+                            }}
+                          >
+                            {s.assetSymbol} — {s.name}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div> */}
+                </td>
+                <td>
+                  <input
+                    type="date"
+                    name="dueDate"
+                    value={'mock bonds data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    name="quantityChange"
+value={'mock bonds data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                    placeholder="Quantity"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="pricePerUnit"
+value={'mock bonds data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                    placeholder="Period"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <input
+          type="radio"
+          name="tabs"
+          id="tab-realestate"
+          onChange={() => setSelectedTab("tab-realestate")}
+          checked={selectedTab === "tab-realestate"}
+        />
+        <label htmlFor="tab-realestate">Real estate</label>
+        <div className="tab">
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Account</th>
+                <th scope="col">Due Date</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Period</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Account">Real estate - 3412</td>
+                <td data-label="Due Date">04/01/2016</td>
+                <td data-label="Quantity">$1,190</td>
+                <td data-label="Period">03/01/2016 - 03/31/2016</td>
+              </tr>
+              <tr>
+                <td scope="row" data-label="Account">
+                  Real estate - 6076
+                </td>
+                <td data-label="Due Date">03/01/2016</td>
+                <td data-label="Quantity">$2,443</td>
+                <td data-label="Period">02/01/2016 - 02/29/2016</td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="asset-autocomplete">
+                    {/* <input
+                      type="text"
+                      name="asset"
+                      value={searchTerm}
+                      onChange={(e) => {
+                        setAutocompleteEnabled(true);
+                        setSearchTerm(e.target.value);
+                      }}
+                      onKeyDown={(e) => {
+                        // if (e.key === "Enter") handleAddAsset();
+                      }}
+                      required
+                      placeholder="Asset"
+                      autoComplete="off"
+                    /> */}
+                  </div>
+                </td>
+                <td>
+                  <input
+                    type="date"
+                    name="dueDate"
+                    value={'mock real estate data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    name="quantityChange"
+value={'mock real estate data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                    placeholder="Quantity"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="pricePerUnit"
+value={'mock real estate data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                    placeholder="Period"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <input
+          type="radio"
+          name="tabs"
+          id="tab-crypto"
+          onChange={() => setSelectedTab("tab-crypto")}
+          checked={selectedTab === "tab-crypto"}
+        />
+        <label htmlFor="tab-crypto">Crypto</label>
+        <div className="tab">
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Account</th>
+                <th scope="col">Due Date</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Period</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Account">Crypto - 3412</td>
+                <td data-label="Due Date">04/01/2016</td>
+                <td data-label="Quantity">$1,190</td>
+                <td data-label="Period">03/01/2016 - 03/31/2016</td>
+              </tr>
+              <tr>
+                <td scope="row" data-label="Account">
+                  Crypto - 6076
+                </td>
+                <td data-label="Due Date">03/01/2016</td>
+                <td data-label="Quantity">$2,443</td>
+                <td data-label="Period">02/01/2016 - 02/29/2016</td>
+              </tr>
+              <tr>
+                <td>
+                  <div className="asset-autocomplete">
+                    {/* <input
+                      type="text"
+                      name="asset"
+                      value={searchTerm}
+                      onChange={(e) => {
+                        setAutocompleteEnabled(true);
+                        setSearchTerm(e.target.value);
+                      }}
+                      onKeyDown={(e) => {
+                        // if (e.key === "Enter") handleAddAsset();
+                      }}
+                      required
+                      placeholder="Asset"
+                      autoComplete="off"
+                    /> */}
+                  </div>
+                </td>
+                <td>
+                  <input
+                    type="date"
+                    name="dueDate"
+                    value={'mock crypto data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                  />
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    name="quantityChange"
+value={'mock crypto data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                    placeholder="Quantity"
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    name="pricePerUnit"
+value={'mock crypto data'}
+                    // onChange={handleChange}
+                    onKeyDown={(e) => {
+                      // if (e.key === "Enter") handleAddAsset();
+                    }}
+                    required
+                    placeholder="Period"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </section>
   );
